f117e20ace968c1c669f32c6a0a80e5f000330f1
ggemre <gmoore1@byu.edu>
2022-11-12 21:12:21 +0000

    Remove shell scripts

--- scripts/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for app in $(ls /home/dme/data); do
-  cd /home/dme/data/$app
-  sudo docker-compose up -d
-done
-
--- scripts/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for app in $(ls /home/dme/data); do
-  cd /home/dme/data/$app
-  sudo docker-compose down
-done
-
-sudo shutdown
